91a2b66ec96d0b379830742c573bba78b3732534
sowmya sri <sowmyasri23164@gmail.com>
2023-03-06 01:38:04 -0400

    Application with Timeline component creation

---
+++ src/components/Timeline.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import {ref} from 'vue'
+
+const periods=["Today","This Week", "This Month"] as const
+type Period =typeof periods[number]
+
+const selectedPeriod= ref<Period>("Today")
+function selectPeriod(period: Period){
+    console.log(period)
+    selectedPeriod.value=period
+}
+</script>
+
+<template>
+    <nav class="is-primary panel">
+        {{ selectedPeriod }}
+        <span class="panel-tabs">
+            <a v-for="period of periods"
+            :key="period"
+            :class="{'is-active': period===selectedPeriod}"
+            @click="selectPeriod(period)">
+                {{ period }}
+            </a>
+        </span>
+    </nav>
+</template>
